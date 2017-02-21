53a998459d30d73a3b481db1f2dc62a0c4820f1b
Chris Moreton <chris@netsensia.com>
2017-02-21 16:01:54 +0000

    Add admin scope

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,5 +27,9 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+        
+        Passport::tokensCan([
+            'admin' => 'Full read/write access',
+        ]);
     }
 }
