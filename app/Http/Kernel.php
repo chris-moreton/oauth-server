3dd27bf56067edf2344b499cc0383b57d066bf5d
Chris Moreton <chris@netsensia.com>
2017-03-21 11:34:59 +0000

    Assume empty scopes equals * scope for client_credentials grant

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,6 +58,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'client_credentials' => \Laravel\Passport\Http\Middleware\CheckClientCredentials::class,
+        'client_credentials' => \App\Extensions\CheckClientCredentials::class,
     ];
 }
