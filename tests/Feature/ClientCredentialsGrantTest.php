b17349d1c37395003cbb8818396d1e5469c4cdf6
Chris Moreton <chris@netsensia.com>
2017-03-28 11:42:28 +0100

    Add client credentials grant tests

---
+++ tests/Feature/ClientCredentialsGrantTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ClientCredentialsGrantTest extends TestCase
+{
+    private $endpoint = '/oauth/token';
+    
+    public function testClientCredentialsGrantAllScopes()
+    {
+        $clientCredentialsGrantClient = $this->getClientCredentialsDetails();
+      
+        $response = $this->post($this->endpoint, [
+                'grant_type' => 'client_credentials',
+                'client_id' => $clientCredentialsGrantClient->id,
+                'client_secret' => $clientCredentialsGrantClient->secret,
+                'scope' => '*',
+            ],
+            $this->getPostHeaders()
+        );
+            
+        $response->assertStatus(200);
+        $response->assertJson($this->getBearerTokenJson());
+    }
+    
+    public function testClientCredentialsGrantWithSelectedScopes()
+    {
+        $clientCredentialsGrantClient = $this->getClientCredentialsDetails();
+    
+        $response = $this->post($this->endpoint, [
+                'grant_type' => 'client_credentials',
+                'client_id' => $clientCredentialsGrantClient->id,
+                'client_secret' => $clientCredentialsGrantClient->secret,
+                'scope' => 'user-read user-update',
+            ],
+            $this->getPostHeaders()
+        );
+    
+        $response->assertStatus(200);
+        $response->assertJson($this->getBearerTokenJson());
+    }
+    
+    public function testClientCredentialsGrantWithInvalidScopes()
+    {
+        $clientCredentialsGrantClient = $this->getClientCredentialsDetails();
+    
+        $response = $this->post($this->endpoint, [
+                'grant_type' => 'client_credentials',
+                'client_id' => $clientCredentialsGrantClient->id,
+                'client_secret' => $clientCredentialsGrantClient->secret,
+                'scope' => 'user-read,user-update',
+            ],
+            $this->getPostHeaders()
+        );
+        
+        $response->assertJson($this->getInvalidScopesJson());
+        $response->assertStatus(400);
+    }
+
+}
